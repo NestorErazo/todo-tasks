32f74ce0f74a17b5fb160b7b32a1d7c9bdd6049f
njerazo <njerazo@gmail.com>
2024-08-05 21:07:48 -0500

    Creacion Api 2

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const api=require('./api');
 const port = process.env.PORT || 3000;
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.listen(port, function () {
     console.log("http://localhost:3000/ " + port);
